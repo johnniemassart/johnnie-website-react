2ee6ac27b2ea237ccc308b6b64bb9a3fb3029410
johnnie massart <109813306+johnniemassart@users.noreply.github.com>
2023-01-11 12:55:46 -0600

    Update HomeImages.jsx

--- src/Components/HomeImages.jsx
+++ src/Components/HomeImages.jsx
@@ -2,8 +2,8 @@ import { useState, useEffect } from "react";
 
 export const HomeImages = () => {
     const images = [
-        {src: '../public/images/image-board.webp', alt: 'image board', id: 1, num: 'one'},
-        {src: '.../public/images/nyc-stairs.webp', alt: 'nyc stairs', id: 2,  num: 'two'},
+        {src: './images/image-board.webp', alt: 'image board', id: 1, num: 'one'},
+        {src: './images/nyc-stairs.webp', alt: 'nyc stairs', id: 2,  num: 'two'},
         {src: './images/nyc-streets-j.webp', alt: 'nyc streets', id: 3, num: 'three'},
         {src: './images/palm-trees.webp', alt: 'palm trees', id: 4, num: 'four'},
         {src: './images/pmoa.webp', alt: 'philadelphia museum of art', id: 5, num: 'five'}
@@ -22,7 +22,7 @@ export const HomeImages = () => {
     return (
             <div className="img-container">
                 {images.map((image) => {
-                    return <img src={require(image.src)} alt={image.alt} key={image.id} className={`img ${image.num} ${position ? 'positioning' : ''}`}/>
+                    return <img src={image.src} alt={image.alt} key={image.id} className={`img ${image.num} ${position ? 'positioning' : ''}`}/>
                 })}
             </div>
     );
